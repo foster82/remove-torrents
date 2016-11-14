2bef7a5590533b8aaa67df3e3535659b563d4cd9
Martin <martin.liversidge@gmail.com>
2016-11-14 05:43:14 +0000

    Create remove-torrents.sh

---
+++ remove-torrents.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# the folder to move completed downloads to
+
+# port, username, password
+# SERVER="9091 --auth transmission:transmission"
+SERVER="9091"
+REMOTE="/usr/local/transmission/bin/transmission-remote"
+# use transmission-remote to get torrent list from transmission-remote list
+# use sed to delete first / last line of output, and remove leading spaces
+# use cut to get first field from each line
+TORRENTLIST=`$REMOTE $SERVER --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1`
+
+$REMOTE $SERVER --list 
+
+# for each torrent in the list
+for TORRENTID in $TORRENTLIST
+do
+    echo Processing : $TORRENTID 
+
+    # check if torrent download is completed
+    DL_COMPLETED=`$REMOTE $SERVER --torrent $TORRENTID --info | grep "Percent Done: 100%"`
+
+    # check torrents current state is
+    STATE_STOPPED=`$REMOTE $SERVER --torrent $TORRENTID --info | grep "State: Seeding\|Stopped\|Finished\|Idle"`
+    echo $STATE_STOPPED
+
+    # if the torrent is "Stopped", "Finished", or "Idle after downloading 100%"
+    if [ "$DL_COMPLETED" -a "$STATE_STOPPED" ]; then
+        # move the files and remove the torrent from Transmission
+        echo "Torrent #$TORRENTID is completed"
+        echo "Removing torrent from list"
+        $REMOTE $SERVER --torrent $TORRENTID --remove
+    else
+        echo "Torrent #$TORRENTID is not completed. Ignoring."
+    fi
+done
