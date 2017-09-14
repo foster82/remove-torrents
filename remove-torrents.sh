81328f0d93ac92411fa7e5ced2f0d8d14353d016
foster82 <ilovemrsbaum@yahoo.co.uk>
2017-09-14 02:19:14 +0100

    Update remove-torrents.sh
    updated code to current version

--- remove-torrents.sh
+++ remove-torrents.sh
@@ -5,24 +5,28 @@
 # port, username, password
 # SERVER="9091 --auth transmission:transmission"
 SERVER="9091"
-REMOTE="/usr/local/transmission/bin/transmission-remote"
+REMOTE="sudo transmission-remote"
 # use transmission-remote to get torrent list from transmission-remote list
 # use sed to delete first / last line of output, and remove leading spaces
 # use cut to get first field from each line
-TORRENTLIST=`$REMOTE $SERVER --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1`
+#Liat of completed torrents emailed
+$REMOTE -l | awk '{print $10 }' >> /home/pi/completed$(date +%F).txt
+echo "Completed Torrents" | mutt  -s  "Completed torrents" ilovemrsbaum@gmail.com -a /home/pi//home/pi/completed$(date +%F).txt
 
-$REMOTE $SERVER --list 
+TORRENTLIST=`$REMOTE --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1`
+
+$REMOTE --list
 
 # for each torrent in the list
 for TORRENTID in $TORRENTLIST
 do
-    echo Processing : $TORRENTID 
+    echo Processing : $TORRENTID
 
     # check if torrent download is completed
-    DL_COMPLETED=`$REMOTE $SERVER --torrent $TORRENTID --info | grep "Percent Done: 100%"`
+    DL_COMPLETED=`$REMOTE  --torrent $TORRENTID --info | grep "Percent Done: 100%"`
 
     # check torrents current state is
-    STATE_STOPPED=`$REMOTE $SERVER --torrent $TORRENTID --info | grep "State: Seeding\|Stopped\|Finished\|Idle"`
+    STATE_STOPPED=`$REMOTE  --torrent $TORRENTID --info | grep "State: Seeding\|Stopped\|Finished\|Idle"`
     echo $STATE_STOPPED
 
     # if the torrent is "Stopped", "Finished", or "Idle after downloading 100%"
@@ -30,7 +34,7 @@ do
         # move the files and remove the torrent from Transmission
         echo "Torrent #$TORRENTID is completed"
         echo "Removing torrent from list"
-        $REMOTE $SERVER --torrent $TORRENTID --remove
+        $REMOTE --torrent $TORRENTID --remove
     else
         echo "Torrent #$TORRENTID is not completed. Ignoring."
     fi
