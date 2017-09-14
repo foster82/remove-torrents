6e07da51f9cd9afa236c06654ae3d3661ecb4b84
foster82 <ilovemrsbaum@yahoo.co.uk>
2017-09-14 02:56:39 +0100

    Update remove-torrents.sh

--- remove-torrents.sh
+++ remove-torrents.sh
@@ -13,7 +13,7 @@ REMOTE="sudo transmission-remote"
 $REMOTE -l | awk '{print $10 }' >> /home/pi/completed$(date +%F).txt
 echo "Completed Torrents" | mutt  -s  "Completed torrents" ilovemrsbaum@gmail.com -a /home/pi/completed$(date +%F).txt
 
-TORRENTLIST=`$REMOTE --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1 |grep -v '*'`
+TORRENTLIST=`$REMOTE --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1 | grep -v '*'`
 
 $REMOTE --list
 
